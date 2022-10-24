634e1ca786738801512979e8ffe562d43ffda009
matiasmeroi <matiasmeroi@hotmail.com>
2022-10-23 21:15:58 -0300

    Información básica del player

--- core/src/mati/vamps/players/PlayerInfo.kt
+++ core/src/mati/vamps/players/PlayerInfo.kt
@@ -1,15 +1,23 @@
 package mati.vamps.players
 
-class PlayerInfo {
+data class PlayerInfo (
+    var name: String = "Unnamed",
+    var description: String = "Empty Description",
+    var maxHealth: Float = 100f,
+    var might: Float = 1f,
+    var armor: Float = 0f,
+    var moveSpeed: Float = 1f,
+    var recovery: Float = 0f,
+    var pickupRadius: Float = 30f,
+    var weaponCooldownMultiplier: Float = 1f,
+    var type: PlayerType = PlayerType.NONE,
+    var textureName: String = "None",
 
-    var name: String = "Unnamed"
-    var description: String = "Empty Description"
-    var might: Float = 1f
-    var armor: Float = 0f
-    var moveSpeed: Float = 1f
-    var recovery: Float = 0f
-    var pickupRadius: Float = 30f
-    var weaponCooldownMultip: Float = 1f
+    // sprite dimensions
+    var _w: Float = 52f,
+    var _h: Float = 52f,
 
-
-}
+    // collision rectangle dimensions
+    var _cw: Float = 32f,
+    var _ch: Float = 32f
+)
