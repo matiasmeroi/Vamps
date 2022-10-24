f6c9480716b04a2cc0932ad2c9bc98a788ada5fb
matiasmeroi <matiasmeroi@hotmail.com>
2022-10-23 21:18:31 -0300

    Bandera debug, json, y  random

--- core/src/mati/vamps/Utils.kt
+++ core/src/mati/vamps/Utils.kt
@@ -1,10 +1,17 @@
 package mati.vamps
 
+import com.badlogic.gdx.utils.Json
 import mati.vamps.screens.GameScreen
 import mati.vamps.screens.MenuScreen
+import java.util.*
 
 object Utils {
 
+    const val DEBUG = true
     const val ATLAS_PATH = "gfx/atlas.atlas"
+
+    val json = Json()
+    val r = Random()
+
 
 }
