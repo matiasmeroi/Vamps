b017c220d8bdfbcc126e2b6c9808fc21cc084998
matiasmeroi <matiasmeroi@hotmail.com>
2022-10-23 21:17:49 -0300

    Agregado jugador y mapa

--- core/src/mati/vamps/screens/GameScreen.kt
+++ core/src/mati/vamps/screens/GameScreen.kt
@@ -4,18 +4,23 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input
 import com.badlogic.gdx.Screen
 import com.badlogic.gdx.graphics.OrthographicCamera
-import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
+import com.badlogic.gdx.math.Rectangle
+import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.scenes.scene2d.Stage
-import com.badlogic.gdx.utils.Scaling
 import com.badlogic.gdx.utils.ScreenUtils
 import com.badlogic.gdx.utils.viewport.FitViewport
-import com.badlogic.gdx.utils.viewport.ScalingViewport
+import mati.vamps.Entity
+import mati.vamps.Utils
 import mati.vamps.Vamps
+import mati.vamps.events.EventManager
+import mati.vamps.events.VEvent
+import mati.vamps.map.Map
 import mati.vamps.players.Player
+import mati.vamps.players.PlayerFactory
+import mati.vamps.players.PlayerType
 
-class GameScreen : Screen {
+class GameScreen : Screen, EventManager.VEventListener {
 
     val mainStage = Stage(
         FitViewport(Gdx.graphics.width + 0f, Gdx.graphics.height + 0f,
@@ -24,29 +29,71 @@ class GameScreen : Screen {
 
     )
 
-    val sr = ShapeRenderer()
+    private val sr = ShapeRenderer()
+    private val playerFactory = PlayerFactory()
+    private val player: Player
+    private val map = Map(mainStage)
     init {
-        mainStage.camera.position.set(mainStage.camera.viewportWidth/2,mainStage.camera.viewportHeight/2,0f);
+        EventManager.subscribe(this)
 
-        mainStage.addActor(Player())
+        playerFactory.load()
+
+        player = playerFactory.create(PlayerType.GREG)
+        mainStage.addActor(player)
     }
 
     override fun show() {
 
     }
 
+    private fun update() {
+        player.handleInput()
+
+        val mx = Gdx.input.getX()
+        val my = Gdx.input.getY()
+        val scr = Vector2(mx+0f, my+0f)
+        val stg = mainStage.screenToStageCoordinates(scr)
+
+
+        val mr = Rectangle(stg.x, stg.y, 1f, 1f)
+        if(player.getRect().overlaps(mr)) {
+            println("OK")
+        }
+
+
+
+        map.update()
+
+    }
+
+    private fun debugDraw() {
+        Vamps.sr().projectionMatrix = mainStage.camera.combined
+
+        Vamps.sr().begin(ShapeRenderer.ShapeType.Line)
+
+        for(act in mainStage.actors) {
+            if(act is Entity) {
+                act.drawColRect()
+            }
+        }
+
+        Vamps.sr().end()
+    }
+
     override fun render(delta: Float) {
+        update()
+
         ScreenUtils.clear(0f, 0f, 0f, 1f)
 
-        sr.begin(ShapeRenderer.ShapeType.Filled)
-        sr.setColor(1f, 1f, 1f, 1f)
-        sr.rect(0f, 0f, mainStage.viewport.worldWidth, mainStage.viewport.worldHeight)
-        sr.end()
+        map.draw()
 
-        mainStage.getBatch().setProjectionMatrix(mainStage.getCamera().combined);
-        mainStage.getCamera().update();
+        mainStage.camera.position.set(player.getX(), player.y, 0f);
+        mainStage.camera.update();
+        mainStage.batch.setProjectionMatrix(mainStage.getCamera().combined);
 
         mainStage.draw()
+
+        debugDraw()
 
         if(Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
             Gdx.app.exit()
@@ -55,8 +102,6 @@ class GameScreen : Screen {
 
     override fun resize(width: Int, height: Int) {
         mainStage.viewport.update(width, height)
-        mainStage.camera.position.set(mainStage.camera.viewportWidth / 2,mainStage.camera.viewportHeight / 2,0f);
-
     }
 
     override fun pause() {
@@ -73,5 +118,16 @@ class GameScreen : Screen {
 
     override fun dispose() {
         mainStage.dispose()
+    }
+
+    override fun onVEvent(event: VEvent, params: String) {
+        when(event) {
+//            VEvent.PLAYER_MOVED_BY -> {
+//                val p = params.split(EventManager.PARAM_SEP)
+//                val dx = Utils.json.fromJson(Float::class.java, p[0])
+//                val dy = Utils.json.fromJson(Float::class.java, p[1])
+//                mainStage.camera.translate(dx, dy, 0f)
+//            }
+        }
     }
 }
