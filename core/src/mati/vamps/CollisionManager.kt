a4aa9f622bed76075f14b69df115909c8a63f8f6
matiasmeroi <matiasmeroi@hotmail.com>
2022-10-24 18:31:18 -0300

    Collision jugador enemigo y separacion enemy

---
+++ core/src/mati/vamps/CollisionManager.kt
@@ -0,0 +1,69 @@
+package mati.vamps
+
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.scenes.scene2d.Actor
+import com.badlogic.gdx.utils.Array
+import mati.vamps.enemies.Enemy
+import mati.vamps.events.EventManager
+import mati.vamps.events.VEvent
+import mati.vamps.players.Player
+import com.badlogic.gdx.utils.Array as GdxArray
+
+class CollisionManager {
+
+    companion object {
+        const val ENEMY_SEPARATION_ACC = 0.5f
+    }
+
+    fun run(player: Player, enemyList: GdxArray<Enemy>) {
+        separateEnemies(enemyList)
+        player2enemy(player, enemyList)
+    }
+
+
+    fun player2enemy(player: Player, enemyList: GdxArray<Enemy>) {
+        val v = Vector2()
+       for(enemy in enemyList) {
+           v.set(player.x, player.y)
+           v.sub(enemy.getPosition())
+
+           if(v.len2() < 100 && player.getRect().overlaps(enemy.getRect())) {
+               enemy.stop()
+               EventManager.announce(VEvent.PLAYER_ENEMY_COLLISION, Utils.json.toJson(enemy.getDmgPerFrame()))
+           }
+
+
+       }
+    }
+
+
+    fun separateEnemies(enemyList: GdxArray<Enemy>) {
+        var nActors = enemyList.size
+        var iters = (nActors * 0.9).toInt()
+
+        while(iters > 0) {
+            val idx1 = Utils.r.nextInt(nActors)
+            var idx2 = idx1
+
+            while(idx1 == idx2) idx2 = Utils.r.nextInt(nActors)
+
+            val e1 = enemyList.get(idx1)
+            val e2 = enemyList.get(idx2)
+
+            val diff = e1.getPosition().sub(e2.getPosition())
+
+            if(diff.len() > 50)  {
+                iters--
+                continue
+            }
+
+            diff.nor().scl(ENEMY_SEPARATION_ACC)
+
+            e1.addToAcceleration(diff.x, diff.y)
+            e2.addToAcceleration(-diff.x, -diff.y)
+
+            iters--
+        }
+    }
+
+}
