f949d0df5032714cf1ecc375b268bc6110740558
matiasmeroi <matiasmeroi@hotmail.com>
2022-10-24 18:34:02 -0300

    Más eventos

--- core/src/mati/vamps/events/VEvent.kt
+++ core/src/mati/vamps/events/VEvent.kt
@@ -2,8 +2,11 @@ package mati.vamps.events
 
 enum class VEvent  {
 
+    PLAYER_POSITION,
     PLAYER_MOVED_BY,
-    PLAYER_ENTITY_COLLISION,
+    PLAYER_ENEMY_COLLISION,
 
+    ENEMY_KILLED,
+    ENEMY_REMOVED
 
 }
