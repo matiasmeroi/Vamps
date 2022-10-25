1e6795508727b0b77d3ae58878688daa293391e6
matiasmeroi <matiasmeroi@hotmail.com>
2022-10-25 20:25:53 -0300

    Creado

---
+++ core/src/mati/vamps/particles/DmgParticle.kt
@@ -0,0 +1,25 @@
+package mati.vamps.particles
+
+import com.badlogic.gdx.math.Vector2
+import com.kotcrab.vis.ui.widget.VisLabel
+import mati.vamps.Utils
+
+class DmgParticle(val num: Int) : VisLabel("$num") {
+
+    init {
+        this.setColor(1f, 0f, 0f, 1f)
+        this.setFontScale(1.5f)
+    }
+
+    private var timer = Utils.r.nextInt(10) + 30
+
+    override fun act(delta: Float) {
+        super.act(delta)
+        timer --
+        if(timer <= 0) remove()
+    }
+
+
+
+
+}
