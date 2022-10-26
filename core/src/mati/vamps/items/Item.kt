7f9695616e4bdad9c5d9f77aa6317833fef2a0e2
matiasmeroi <matiasmeroi@hotmail.com>
2022-10-26 15:41:42 -0300

    Clase Item

---
+++ core/src/mati/vamps/items/Item.kt
@@ -0,0 +1,22 @@
+package mati.vamps.items
+
+import com.badlogic.gdx.graphics.g2d.Batch
+import mati.vamps.Entity
+
+class Item : Entity() {
+
+    enum class Type {
+        NONE,
+        DIAMOND_BLUE_10
+    }
+
+    fun onPickUpEffect() : ItemEffect {
+        return (info as ItemInfo).effect
+    }
+
+    override fun draw(batch: Batch?, parentAlpha: Float) {
+        super.draw(batch, parentAlpha)
+        drawCentered(batch!!, texture)
+    }
+
+}
