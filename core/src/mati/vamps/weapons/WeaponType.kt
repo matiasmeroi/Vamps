7bab157eb72e71563f1bdeaf72404e9a0ed1f7c7
matiasmeroi <matiasmeroi@hotmail.com>
2022-10-28 14:46:52 -0300

    Agregados tipos de armas WHIP Y HOLY_WATER

--- core/src/mati/vamps/weapons/WeaponType.kt
+++ core/src/mati/vamps/weapons/WeaponType.kt
@@ -1,7 +1,9 @@
 package mati.vamps.weapons
 
-enum class WeaponType {
+enum class WeaponType(val desciption: String = "") {
     NONE,
-    KNIVES,
-    GARLIC,
+    KNIVES(desciption = "Throw knives at enemies"),
+    GARLIC(desciption = "Hurts nearby enemies"),
+    WHIP(desciption = "Hits several enemies at a time"),
+    HOLY_WATER(desciption = "Creates areas of damage")
 }
