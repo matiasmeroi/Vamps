e738f77ca94f156c7138e9125a6eebbced9da3a9
matiasmeroi <matiasmeroi@hotmail.com>
2022-10-28 14:50:46 -0300

    Se puede cambiar la cantidad de hits de un ThrowableProjectile

--- core/src/mati/vamps/weapons/projectiles/ThrowableProjectile.kt
+++ core/src/mati/vamps/weapons/projectiles/ThrowableProjectile.kt
@@ -2,13 +2,13 @@ package mati.vamps.weapons.projectiles
 
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.math.Vector2
-import mati.vamps.Utils
+import mati.vamps.utils.Utils
 import mati.vamps.enemies.Enemy
 
 class ThrowableProjectile(val dir: Vector2) : Projectile() {
 
-    var timer: Int = 0
-
+    private var timer: Int = 0
+    private var hitsLeft = 1
     override fun onInitialize() {
         timer = (info as ProjectileInfo).timeOnScreen
 
@@ -16,16 +16,22 @@ class ThrowableProjectile(val dir: Vector2) : Projectile() {
         if(dir.x != 0f && dir.y != 0f) velocity.scl(Utils.DIAG_SPEED_MULTIPLIER)
     }
 
+    fun setMaxHits(h : Int) : ThrowableProjectile {
+        hitsLeft = h
+        return this
+    }
+
     override fun isDealingDmg(): Boolean {
-        return true
+        return hitsLeft > 0
     }
 
     override fun toRemove(): Boolean {
-        return timer <= 0
+        return timer <= 0 || hitsLeft <= 0
     }
 
     override fun onEnemyHit(enemy: Enemy) {
         timer = 0
+        hitsLeft--
     }
 
     override fun onUpdate(areaMultiplier: Float) {
@@ -35,7 +41,7 @@ class ThrowableProjectile(val dir: Vector2) : Projectile() {
     }
 
     override fun draw(areaMultiplier: Float, batch: Batch) {
-        drawCenteredAndScale(batch, texture, areaMultiplier, dir)
+        drawCenteredAndScale(batch, texture, areaMultiplier)
     }
 
 
