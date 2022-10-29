85688afeeae14f35e0bc3e9369b1218b403b1f1e
matiasmeroi <matiasmeroi@hotmail.com>
2022-10-29 11:04:36 -0300

    Agregado tag a ThrowableProjectile y campo customTarget(no hace una mierda -_-)

--- core/src/mati/vamps/weapons/projectiles/ThrowableProjectile.kt
+++ core/src/mati/vamps/weapons/projectiles/ThrowableProjectile.kt
@@ -1,19 +1,26 @@
 package mati.vamps.weapons.projectiles
 
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.math.Vector2
 import mati.vamps.utils.Utils
 import mati.vamps.enemies.Enemy
 
-class ThrowableProjectile(val dir: Vector2) : Projectile() {
+class ThrowableProjectile(private var dir: Vector2, val customTarget: Boolean = false)  : Projectile() {
+    
+    
+    companion object {
+        val TAG = ThrowableProjectile::class.java.getSimpleName()
+    }
 
     private var timer: Int = 0
     private var hitsLeft = 1
     override fun onInitialize() {
         timer = (info as ProjectileInfo).timeOnScreen
 
-        velocity.set(dir.x * (info as ProjectileInfo).speed, dir.y * (info as ProjectileInfo).speed)
-        if(dir.x != 0f && dir.y != 0f) velocity.scl(Utils.DIAG_SPEED_MULTIPLIER)
+        velocity.set(dir!!.x * (info as ProjectileInfo).speed, dir!!.y * (info as ProjectileInfo).speed)
+
+        if(!customTarget && dir!!.x != 0f && dir!!.y != 0f) velocity.scl(Utils.DIAG_SPEED_MULTIPLIER)
     }
 
     fun setMaxHits(h : Int) : ThrowableProjectile {
