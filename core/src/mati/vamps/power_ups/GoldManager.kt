583f1cfa367987f5d25bf3e3046aca7d9f93bd05
matiasmeroi <matiasmeroi@hotmail.com>
2022-11-05 21:57:34 -0300

    GoldManager diferencia entre oro total y oro conseguido en esta partida

--- core/src/mati/vamps/power_ups/GoldManager.kt
+++ core/src/mati/vamps/power_ups/GoldManager.kt
@@ -8,24 +8,38 @@ import mati.vamps.utils.Utils
 
 class GoldManager: EventManager.VEventListener, ProfileManager.ProfileListener {
 
-    private var gold = 0
+    private var gold = 1000
+    private var tempGold = 0
 
     init {
         EventManager.subscribe(this)
         ProfileManager.addListener(this)
     }
 
+    fun total() : Int { return gold }
+
+    fun add(dx: Int) { gold += dx }
+
     fun has(amount: Int) : Boolean { return gold >= amount }
+
+    fun earnedThisRound() : Int { return tempGold }
+
     fun spend(amount: Int) {
         gold -= amount
     }
 
     override fun onVEvent(event: VEvent, params: String) {
         when(event) {
+            VEvent.GAME_START -> {
+                tempGold = 0
+            }
+            VEvent.GAME_END -> {
+                gold += tempGold
+            }
             VEvent.ITEM_EFFECT_ACTIVATED -> {
                 when(Utils.json.fromJson(ItemEffect::class.java, params)) {
-                    ItemEffect.COIN_1 -> gold += 1
-                    ItemEffect.COIN_10 -> gold += 10
+                    ItemEffect.COIN_1 -> tempGold += 1
+                    ItemEffect.COIN_10 -> tempGold += 10
                 }
             }
         }
