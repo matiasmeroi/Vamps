ef28168afb3012f1daf4254184c08c75a058ddb8
matiasmeroi <matiasmeroi@hotmail.com>
2022-11-13 20:37:04 -0300

    Agregada enemyWaves a MinuteInfo

--- core/src/mati/vamps/enemies/spawner/MinuteInfo.kt
+++ core/src/mati/vamps/enemies/spawner/MinuteInfo.kt
@@ -1,12 +1,12 @@
 package mati.vamps.enemies.spawner
 
 import mati.vamps.enemies.EnemyType
-import mati.vamps.enemies.spawner.waves.WaveType
+import mati.vamps.enemies.spawner.waves.Waves.Type
 
 data class MinuteInfo (
     var minute: Int = 0,
-    var enemyProbabilityBag : Array<MPI_Entry> = emptyArray(),
-    var enemyWaves : Array<WaveType> = emptyArray()
+    var bag : Array<MPI_Entry> = emptyArray(),
+    var enemyWaves : Array<Type> = emptyArray()
 )
 
 data class MPI_Entry (var num: Int = 1, var type: EnemyType = EnemyType.NONE)
