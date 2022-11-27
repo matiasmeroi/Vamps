b3061693724bf48c1890972b8589d5667d9e092f
matiasmeroi <matiasmeroi@hotmail.com>
2022-11-27 13:20:25 -0300

    DoNothing setea la velocidad a 0

--- core/src/mati/vamps/enemies/behaviors/DoNothing.kt
+++ core/src/mati/vamps/enemies/behaviors/DoNothing.kt
@@ -3,5 +3,7 @@ package mati.vamps.enemies.behaviors
 import mati.vamps.enemies.Enemy
 
 class DoNothing : Behavior() {
-    override fun act(enemy: Enemy) {}
+    override fun act(enemy: Enemy) {
+        enemy.velocity().set(0f, 0f)
+    }
 }
